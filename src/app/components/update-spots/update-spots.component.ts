3f269da5bd18cefda59864c0a537eb9d6da82217
Felipe Bernardo de Oliveira <felipebernardo55@hotmail.com>
2024-09-28 09:32:55 -0300

    wip

--- src/app/components/update-spots/update-spots.component.ts
+++ src/app/components/update-spots/update-spots.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { EstateAndCityApiService } from 'src/app/services/estate-and-city-api.service';
 import { LocalService } from 'src/app/services/local.service';
 import { SnackMessageService } from 'src/app/services/snack-message.service';
 import { environment } from 'src/environments/environment';
@@ -20,7 +21,8 @@ export class UpdateSpotsComponent implements OnInit {
     private localService: LocalService,
     private sanitizer: DomSanitizer,
     private snackMessageService: SnackMessageService,
-    private router: Router
+    private router: Router,
+    private estateAndCityApiService: EstateAndCityApiService
   ) { }
 
   @ViewChild('fileInput') fileInput!: ElementRef;
@@ -29,6 +31,8 @@ export class UpdateSpotsComponent implements OnInit {
   local: Array<any> = [];
   storageUrl: string = environment.BASE_URL_STORAGE;
   images: Array<any> = [];
+  estates: Array<any> = [];
+  cities: Array<any> = [];
 
   ngOnInit(): void {
     if (!this.checkToken()) {
@@ -36,12 +40,14 @@ export class UpdateSpotsComponent implements OnInit {
       this.router.navigate(['/login'])
     } else {
       this.getLocals();
+      this.getEstates();
     }
   }
 
   getLocals(): void {
     this.localService.readLocalBySkaterId(this.getIdUrl()).subscribe((resp: any) => {
       this.local = resp;
+      this.changeCities(this.local[0].address_estate);
       setTimeout(() => {
         this.loader = false;
       }, 1000)
@@ -116,5 +122,17 @@ export class UpdateSpotsComponent implements OnInit {
 
   checkToken(): boolean {
     return sessionStorage.getItem('token') != null;
+  }
+
+  getEstates(): void {
+    this.estateAndCityApiService.getEstate().subscribe((resp: any) => {
+      this.estates = resp;
+    })
+  }
+
+  changeCities(estate: string): void {
+    this.estateAndCityApiService.getCitiesByEstate(estate).subscribe((resp: any) => {
+      this.cities = resp;
+    })
   }
 }
